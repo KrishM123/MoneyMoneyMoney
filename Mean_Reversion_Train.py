3335196b3ae724678da61a935b2787bc0d4fb16a
KrishM123 <kahnscatterkrish@gmail.com>
2024-03-05 22:17:10 -0500

    finished algo, need to figure out loading

--- Mean_Reversion_Train.py
+++ Mean_Reversion_Train.py
@@ -18,17 +18,8 @@ def create_model():
         tf.keras.layers.Dense(1),
     ])
 
-time_effect1 = lambda L, x: 1-(x/L)
-time_effect2 = lambda L, x: L/(x+L)
-time_effect3 = lambda L, x: (-1/(L**2))(x**2)+1
-time_effect3 = lambda L, x: -1/((x-L)**2)
 
-def train(ticker):
-    goog = yf.Ticker(ticker)
-    historic_price = yf.download(ticker)
-    prices = historic_price['Adj Close']
-
-    MAX_HOLDING = 100
+def find_answers(prices, MAX_HOLDING):
     answer = []
     for pos1 in range(len(prices) - MAX_HOLDING):
         ans = 0
@@ -40,45 +31,54 @@ def train(ticker):
     for pos in range(1, len(answer)):
         dp_factors.append(max(abs(answer[pos]), abs(dp_factors[-1])))
         answer[pos] /= dp_factors[pos]
-    answer = answer[MAX_HOLDING:]
-    prices = prices[MAX_HOLDING:]
+    answer[0] = 1
+    return answer
 
-    sma10 = get_sma(prices, 10)
-    sma30 = get_sma(prices, 30)
-    sma50 = get_sma(prices, 50)
-    sma100 = get_sma(prices, 100)
-    sd10 = get_sd(prices, 10)
-    sd30 = get_sd(prices, 30)
-    sd50 = get_sd(prices, 50)
-    sd100 = get_sd(prices, 100)
+
+time_effect1 = lambda L, x: 1-(x/L)
+time_effect2 = lambda L, x: L/(x+L)
+time_effect3 = lambda L, x: (-1/(L**2))(x**2)+1
+time_effect3 = lambda L, x: -1/((x-L)**2)
+
+
+def train(save_name, training_prices, MAX_HOLDING=100, LONGEST_SMA=100):
+    answer = find_answers(training_prices, MAX_HOLDING)
+
+    sma10 = get_sma(training_prices, 10)
+    sma30 = get_sma(training_prices, 20)
+    sma50 = get_sma(training_prices, 50)
+    sma100 = get_sma(training_prices, 100)
+    sd10 = get_sd(training_prices, 10)
+    sd30 = get_sd(training_prices, 20)
+    sd50 = get_sd(training_prices, 50)
+    sd100 = get_sd(training_prices, 100)
+
+    if MAX_HOLDING < LONGEST_SMA:
+        answer = answer[LONGEST_SMA - MAX_HOLDING:]
 
     x = []
     y = []
-    for pos in range(0, len(answer)):
-        temp = [0] * 200
+    for pos in range(0, len(training_prices) - LONGEST_SMA):
         x.append([sma10[pos], sma30[pos], sma50[pos], sma100[pos], sd10[pos], sd30[pos], sd50[pos], sd100[pos]])
         ans = (answer[pos] + 1) / 2
         y.append(ans)
     x = np.array(x)
     y = np.array(y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0, train_size = .70)
-    
     model = create_model()
     model.compile(optimizer='adam',
-                loss='mean_squared_error',)
-
+                loss='mean_absolute_error',)
     model.fit(x=x_train, 
-          y=y_train, 
-          epochs=20, 
-          validation_data=(x_test, y_test), 
+            y=y_train, 
+            epochs=50, 
+            validation_data=(x_test, y_test), 
     )
-
-    path = 'Models/' + ticker + '/'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    
+    if not os.path.exists(save_name):
+        os.makedirs(save_name)
     else:
-        shutil.rmtree(path)
-        os.makedirs(path)
-    model.save(path)
-    model.save_weights(path)
+        shutil.rmtree(save_name)
+        os.makedirs(save_name)
+    model.save(save_name)
+    model.save_weights(save_name)
